78a545f66bb8a37f8eefd26a66ac51233f8636e0
MedEZZOUAK <ezzouak2001@gmail.com>
2024-04-18 21:49:04 +0100

    added my partenaire part controller and model

---
+++ Views/Partenaire/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <h1 class="navbar-brand">Bricolini</h1>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Mes services</a>
+                    <a class="nav-link" href="#">Mes Intervention</a>
+                    <a class="nav-link" href="#">Mon Profil</a>
+                    <a class="nav-link" href="#">Historique</a>
+                    <a class="nav-link" href="#">Deconnexion</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+
+
+
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
