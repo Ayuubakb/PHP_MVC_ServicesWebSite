b13e4069b6334356c057614018b864f38ff10acf
Ysf AB <abouelalaa.youssef@etu.uae.ac.ma>
2024-04-19 16:34:34 +0000

    Update 3 files
    - /Controllers/Process_signup.php
    - /Views/Authentification/signup.php
    - /Controllers/UserController.php

--- Views/Authentification/signup.php
+++ Views/Authentification/signup.php
@@ -1,38 +1,22 @@
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Inscription</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
-    <h2 class="mt-5">Inscription</h2>
-    <form action="process_signup.php" method="post" enctype="multipart/form-data">
-        <div class="form-group">
-            <label for="LastName">Nom:</label>
-            <input type="text" class="form-control" id="LastName" name="LastName" required>
-        </div>
-        <div class="form-group">
-            <label for="FirstName">Prénom:</label>
-            <input type="text" class="form-control" id="FirstName" name="FirstName" required>
-        </div>
-        <div class="form-group">
-            <label for="Email">Email (Partenaires uniquement):</label>
-            <input type="email" class="form-control" id="Email" name="Email">
-        </div>
-        <div class="form-group">
-            <label for="Telephone">Téléphone (Clients uniquement):</label>
-            <input type="text" class="form-control" id="Telephone" name="Telephone">
-        </div>
-        <div class="form-group">
-            <label for="image">Image (optionnel) :</label>
-            <input type="file" class="form-control" id="image" name="image">
-        </div>
-        <button type="submit" class="btn btn-custom">S'inscrire</button>
-    </form>
-</div>
-</body>
-</html>
-```
+<form action="/user/signup" method="post" enctype="multipart/form-data">
+    
+    <input type="text" name="LastName" placeholder="Nom" required>
+    <input type="text" name="FirstName" placeholder="Prénom" required>
+    <input type="text" name="Email" placeholder="Email" required>
+    <input type="text" name="Telephone" placeholder="Téléphone" required>
+    
+    <!-- Champs spécifiques aux clients -->
+    <input type="text" name="Address" placeholder="Adresse">
+    
+    <!-- Champs spécifiques aux partenaires -->
+    <input type="text" name="Metier" placeholder="Métier">
+    <input type="text" name="Ville" placeholder="Ville">
+    <input type="text" name="Creneaux" placeholder="Créneaux">
+    <input type="number" name="YearExperience" placeholder="Années d'expérience">
+    
+    
+    <!-- Champ pour distinguer client/partenaire -->
+    <input type="hidden" name="type" value="client"> <!-- ou value="partenaire" selon le cas -->
+    
+    <input type="submit" value="S'inscrire">
+</form>
