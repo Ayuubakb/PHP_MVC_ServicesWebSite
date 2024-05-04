9738a45fe2bd76d8c6bb627b44fab3440479e681
MedEZZOUAK <ezzouak2001@gmail.com>
2024-05-04 15:16:19 +0100

    fixed the problem maybe
    This reverts commit 8b3a747b6ed39453fa3d084cbfa8ee9eb7b675ba.

--- Views/Authentification/login.php
+++ Views/Authentification/login.php
@@ -3,23 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="../public/style/Style.css">
+    <title>Login</title>
 </head>
-
 <body>
-<div class="welcome-message">
-        <h1>Bricoliini</h1>
-        <hr>
-    </div>
-    
-    <div class="login-container">
-        <form action="/user/login" method="post" class="login-form">
-            <input type="text" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" value="Login">
-        </form>
-    </div>
+    <h1>Login</h1>
+    <form action="http://localhost/Bricolini/Authentification/login" method="post">
+        <input type="text" name="email" placeholder="Email" required>
+        <input tyape="password" name="password" placeholder="Password" required>
+        <input type="submit" value="Login">
+    </form>
 </body>
 </html>
-
