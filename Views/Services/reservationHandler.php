9098e9524744bb2c24d10ca0f9dbf8f899a195c5
wail <wailmad02@gmail.com>
2024-05-05 16:02:11 +0100

    reserver

--- Views/Services/reservationHandler.php
+++ Views/Services/reservationHandler.php
@@ -1,47 +1,36 @@
-
 <?php
+session_start(); 
 include $_SERVER['DOCUMENT_ROOT'] . '/Bricolini/Views/Services/connexion.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(isset($_SESSION['user_id'])){
+        // print "user_id: ";
         $Id_S = $_POST['Id_S'];
         $Date_reserv = $_POST['Date_reserv'];
-        $Id_C = $_POST['Id_C'];
+        $Id_C = $_SESSION['user_id'];
         $Statuts = $_POST['Statuts'];
 
-<<<<<<< HEAD
-    $Id_S = $_POST['Id_S'];
-    $Date_reserv = $_POST['Date_reserv'];
-
-    $Id_C = $_POST['Id_C'];
-    $Statuts = $_POST['Statuts'];
-
-    $sql = "INSERT INTO reservation (Id_S, Date_reserv, Id_C, Statuts) VALUES (?, ?, ?, ?)";
-
-
-    if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param("isii", $Id_S, $Date_reserv, $Id_C, $Statuts);
-        if ($stmt->execute()) {
-            echo "New record created successfully";
-=======
         $sql = "INSERT INTO reservation (Id_S, Date_reserv, Id_C, Statuts) VALUES (?, ?, ?, ?)";
 
         if ($stmt = $conn->prepare($sql)) {
             $stmt->bind_param("isii", $Id_S, $Date_reserv, $Id_C, $Statuts);
             if ($stmt->execute()) {
-                echo "New record created successfully";
+                echo "nouveau record cree avec succes";
+                $stmt->close();
+                $conn->close();
             } else {
                 echo "Error: " . $stmt->error;
+                $stmt->close();
+                $conn->close();
             }
-            $stmt->close();
->>>>>>> 7f0084180faafa2eb197bc4ff2e4ce4dff0a4c6c
         } else {
             echo "Error preparing statement: " . $conn->error;
+            header('Location: http://localhost/Bricolini/Views/Authentification/login.php');
+            exit(); 
         }
-        $conn->close();
-    }else{
-        echo "no";
-        //header('Location:http://localhost/Bricolini/Authentification/login');
+    } else {
+        header('Location: http://localhost/Bricolini/Views/Authentification/login.php');
+        exit();
     }
 }
 ?>
